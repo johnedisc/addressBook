c1b97c7f641d8f0982383cd9d904404c396e6be6
johnedisc <christopher.johnedis@gmail.com>
2023-01-23 11:59:47 -0800

    create delete contact method

--- scripts.js
+++ scripts.js
@@ -13,6 +13,21 @@ AddressBook.prototype.assignId = function() {
   return this.currentId += 1;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
 // business logic for Contact
 function Contact(firstName,lastName,phoneNumber) {
   this.first = firstName;
@@ -24,10 +39,17 @@ Contact.prototype.fullName = function() {
   return this.first + ' ' + this.last; 
 }
 
+Contact.prototype.update = function(firstName,lastName,phoneNumber) {
+  
+}
+
 let addressBook = new AddressBook();
 let george = new Contact('george','gwedel','483-384-3482');
 let miles = new Contact('miles','davis','432-542-9842');
 addressBook.addContact(george);
 addressBook.addContact(miles);
 
-console.log(addressBook.contacts);
+george.first = 'jorge';
+console.log(addressBook.contacts['1'].first = 'georgie');
+console.log(george)
+
